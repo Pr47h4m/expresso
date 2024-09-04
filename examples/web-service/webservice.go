794ff12a62775f3eed00d5640bd34b094d508c28
Pratham Jaiswal <pr47h4m@gmail.com>
2024-09-04 19:08:57 +0530

    added example for HandleNotFound

--- examples/web-service/webservice.go
+++ examples/web-service/webservice.go
@@ -2,6 +2,7 @@ package webservice
 
 import (
 	"encoding/xml"
+	"net/http"
 
 	"github.com/pr47h4m/expresso"
 )
@@ -57,5 +58,42 @@ func App() *expresso.App {
 
 	app.GET("/api/users/:name/repos", ValidateAPIKey, GetUserRepos)
 
+	app.HandleNotFound(HandleNotFound)
+
 	return &app
+}
+
+func HandleNotFound(ctx *expresso.Context) {
+	ctx.Status(http.StatusNotFound).Formatted(ctx.RawRequest, expresso.Formatted{
+		Text: &expresso.Text{Content: "404 - Not Found"},
+		HTML: &expresso.HTML{Content: "<html><head><title>404 - Not Found</title></head><body>404 - Not Found</body></html>"},
+		JSON: &expresso.JSON{
+			Data: map[string]interface{}{
+				"status": 404,
+				"error":  "Not Found",
+			},
+		},
+		XML: &expresso.XML{
+			Data: struct {
+				XMLName xml.Name `xml:"error"`
+				Status  int      `xml:"status"`
+				Error   string   `xml:"error"`
+			}{
+				Status: 404,
+				Error:  "Not Found",
+			},
+		},
+		YAML: &expresso.YAML{
+			Data: map[string]interface{}{
+				"status": 404,
+				"error":  "Not Found",
+			},
+		},
+		Default: &expresso.JSON{
+			Data: map[string]interface{}{
+				"status": 404,
+				"error":  "Not Found",
+			},
+		},
+	})
 }
