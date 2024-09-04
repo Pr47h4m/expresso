27be90450c319ed824a02dadc3dc46e8d296a074
Pratham Jaiswal <pr47h4m@gmail.com>
2024-09-04 18:19:00 +0530

    updated example

--- examples/go.mod
+++ examples/go.mod
@@ -2,9 +2,9 @@ module expresso_example
 
 go 1.19
 
-require github.com/pr47h4m/expresso v0.0.8
+require github.com/pr47h4m/expresso v0.0.10
 
-replace github.com/pr47h4m/expresso v0.0.8 => ../
+replace github.com/pr47h4m/expresso v0.0.10 => ../
 
 require (
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
--- examples/serve-static/servestatic.go
+++ examples/serve-static/servestatic.go
@@ -1,14 +1,61 @@
 package servestatic
 
 import (
+	"io/fs"
 	"net/http"
+	"strings"
 
 	"github.com/pr47h4m/expresso"
 )
 
+// containsDotFile reports whether name contains a path element starting with a period.
+// The name is assumed to be a delimited by forward slashes, as guaranteed
+// by the http.FileSystem interface.
+func containsDotFile(name string) bool {
+	parts := strings.Split(name, "/")
+	for _, part := range parts {
+		if strings.HasPrefix(part, ".") {
+			return true
+		}
+	}
+	return false
+}
+
+// dotFileHidingFile is the http.File use in dotFileHidingFileSystem.
+// It is used to wrap the Readdir method of http.File so that we can
+// remove files and directories that start with a period from its output.
+type dotFileHidingFile struct {
+	http.File
+}
+
+// dotFileHidingFileSystem is an http.FileSystem that hides
+// hidden "dot files" from being served.
+type dotFileHidingFileSystem struct {
+	http.FileSystem
+}
+
+// Open is a wrapper around the Open method of the embedded FileSystem
+// that serves a 403 permission error when name has a file or directory
+// with whose name starts with a period in its path.
+func (fsys dotFileHidingFileSystem) Open(name string) (http.File, error) {
+	if containsDotFile(name) { // If dot file, return 403 response
+		return nil, fs.ErrPermission
+	}
+
+	file, err := fsys.FileSystem.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	return dotFileHidingFile{file}, err
+}
+
 func App() *expresso.App {
 	app := expresso.DefaultApp()
+
 	app.ServeStatic("/public/*filepath", http.Dir("serve-static/public"))
-	app.ServeStatic("/private/*filepath", http.Dir("serve-static/private"))
+
+	fsys := dotFileHidingFileSystem{http.Dir("serve-static/private")}
+	app.ServeStatic("/private/*filepath", http.FileSystem(fsys))
+
 	return &app
 }
