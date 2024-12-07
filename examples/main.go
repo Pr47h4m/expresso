2156961c7ec2db0fbbca76da13b53b17237e6013
Pratham Jaiswal <pr47h4m@gmail.com>
2024-12-07 17:59:32 +0530

    used callback function while starting server

--- examples/main.go
+++ examples/main.go
@@ -17,16 +17,41 @@ func main() {
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
 	done := make(chan bool, 1)
 
-	go helloworld.App().ListenAndServe(":81")
-	// go helloworld.App().ListenAndServeTLS(":81", "server.crt", "server.key")
-	go basicrouting.App().ListenAndServe(":82")
-	// go basicrouting.App().ListenAndServeTLS(":82", "server.crt", "server.key")
-	go servestatic.App().ListenAndServe(":83")
-	// go servestatic.App().ListenAndServeTLS(":83", "server.crt", "server.key")
-	go webservice.App().ListenAndServe(":84")
-	// go webservice.App().ListenAndServeTLS(":84", "server.crt", "server.key")
+	go helloworld.App().ListenAndServe(":81", nil)
+	// go helloworld.App().ListenAndServeTLS(":81", "server.crt", "server.key", func(err error) {
+	// 	if err != nil {
+	// 		fmt.Println("Unable to start helloworld server on port 81", err)
+	// 	} else {
+	// 		fmt.Println("helloworld server is running on port 81")
+	// 	}
+	// })
+	go basicrouting.App().ListenAndServe(":82", nil)
+	// go basicrouting.App().ListenAndServeTLS(":82", "server.crt", "server.key", func(err error) {
+	// 	if err != nil {
+	// 		fmt.Println("Unable to start basicrouting server on port 81", err)
+	// 	} else {
+	// 		fmt.Println("basicrouting server is running on port 81")
+	// 	}
+	// })
+	go servestatic.App().ListenAndServe(":83", func(err error) {
+		if err != nil {
+			fmt.Println("Unable to start servestatic server on port 81", err)
+		} else {
+			fmt.Println("servestatic server is running on port 81")
+		}
+	})
+	// go servestatic.App().ListenAndServeTLS(":83", "server.crt", "server.key", nil)
+	go webservice.App().ListenAndServe(":84", func(err error) {
+		if err != nil {
+			fmt.Println("Unable to start webservice server on port 81", err)
+		} else {
+			fmt.Println("webservice server is running on port 81")
+		}
+	})
+	// go webservice.App().ListenAndServeTLS(":84", "server.crt", "server.key", nil)
 
 	go func() {
+		fmt.Println("press Ctrl+C to exit.")
 		<-sig
 		fmt.Println("\nCtrl+C pressed. Exiting...")
 		done <- true
